7a6a474e74ca7effc86d9c782b767bfd7e561b61
César Reyes <cesar95rt@gmail.com>
2022-01-19 14:43:29 -0300

    update vendor files

--- vendor/github.com/c95rt/bootcamp-user/grpc/repository/global-vars.go
+++
@@ -1,8 +0,0 @@
-package repository
-
-const (
-	DEFAULT_ACTIVE             bool = true
-	DEFAULT_INACTIVE           bool = false
-	DEFAULT_MAX_RETRIES        int  = 3
-	USER_ADDITIONAL_COLLECTION      = `user_additional`
-)
